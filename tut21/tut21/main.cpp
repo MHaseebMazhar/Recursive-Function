398bb8d7fba4e3ea0cc34a7099133f2d1e4d6ec8
Muhammad Haseeb <126347233+MHaseebMazhar@users.noreply.github.com>
2024-06-25 19:32:55 +0500

    Recursive Function
    implemented the Recursive Function

---
+++ tut21/tut21/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  tut21
+//
+//  Created by Shaista Awan on 25/06/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
